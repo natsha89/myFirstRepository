0a58bf9542353839ccf4666d81ae1ae426cce740
natasha <natasha.shahran@gmail.com>
2023-04-08 18:38:05 +0200

    add comment on line 8.

--- my first job/src/Main.java
+++ my first job/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press ⌥⏎ with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Natasha");
-        System.out.println("wursti889");
+        System.out.println("wursti889");//Thank you for your time!
 
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
