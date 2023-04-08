f180315924ffac6559e3fa53333f96264367fbf4
natasha <natasha.shahran@gmail.com>
2023-04-08 18:14:21 +0200

    I love my dogs

---
+++ my first job/src/Main.java
@@ -0,0 +1,14 @@
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press ⌥⏎ with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Natasha");
+        System.out.println("wursti");
+
+
+        // Press ⌃R or click the green arrow button in the gutter to run the code.
+
+    }
+}
