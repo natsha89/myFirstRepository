13bfa29cc806c1fd3664ddaf6c9f5297d096d864
natasha <natasha.shahran@gmail.com>
2023-04-08 18:28:18 +0200

    dlt. line 9. add new string.

--- my first job/src/Main.java
+++ my first job/src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         // Press ⌥⏎ with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Natasha");
-        System.out.println("wursti");
-        System.out.println("koorosh");
+        System.out.println("wursti889");
 
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
